0a3b418b776e1356fc845057d19cb811c06274e4
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2022-10-12 23:50:55 +0300

    Basic realization of components

--- js-app/validation/authorization.js
+++
@@ -1,5 +0,0 @@
-import {AUTHORIZATION_FORM} from './constants.js';
-import {validateAuthorization} from './validate-forms.js';
-
-const formForAuthorization = document.getElementById(AUTHORIZATION_FORM);
-validateAuthorization(formForAuthorization);
