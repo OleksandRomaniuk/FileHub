71007fcfae202c5819c5f9045a3b7e5cb2280f25
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2022-10-18 22:46:07 +0300

    Refactoring code

--- js-app/validation/validator-service.js
+++ js-app/validation/validator-service.js
@@ -11,25 +11,20 @@ export class ValidatorService {
    * @returns {Promise<void>}
    */
   async validate(config, formData) {
-    const validationsErrors = [];
-    const iterator = config.configs.entries();
-    for (let j = 0; j < config.configs.size; j++) {
-      const configFieldArray = iterator.next().value;
-      const field = configFieldArray[0];
-      const validators = configFieldArray[1];
-
+    const configs = Array.from(config.configs, ([field, validators]) => ({field, validators}));
+    const promises = configs.map(({field, validators}) => {
       const value = formData.get(field);
-
-      for (let i = 0; i < validators.length; i++) {
-        try {
-          await validators[i](value);
-        } catch (error) {
-          validationsErrors.push(new ValidationError(field, error.message));
-        }
-      }
-    }
-    if (validationsErrors.length) {
-      throw new ValidationErrorResult(validationsErrors);
-    }
-  };
+      return validators.map((validator) => validator(value)
+          .catch((error) => {
+            throw new ValidationError(field, error.message);
+          }));
+    }).flat();
+    const promise = Promise.allSettled(promises);
+    return promise.then((results) => {
+      const validatorErrors = results
+          .filter((result) => result.status==='rejected')
+          .map((error) => error.reason);
+      throw new ValidationErrorResult(validatorErrors);
+    });
+  }
 }
