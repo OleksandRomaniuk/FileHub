2a60e6d2af8f9615321717ccd79ae7de38de05dd
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2022-11-25 16:15:48 +0200

    Rewrite tests

--- js-app/src/tests/validation/validatior.test.js
+++ js-app/src/tests/validation/validatior.test.js
@@ -3,45 +3,40 @@ import {validateValueEquals, validateValueLength, validateValueWithRegex} from '
 
 describe('validateValueLength', () => {
   test(`Should return resolved promise is arguments is valid.`, async function() {
-    const testValues = [
+    const validArguments = [
       [6, 'password'],
       [2, 'pas'],
       [10, 'desultoriness'],
       [0, ''],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(validArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of validArguments) {
       const validator = validateValueLength(testValue[0]);
 
-      await validator(testValue[1]);
-      expect(true).toBeTruthy();
+      await expect(validator(testValue[1])).resolves.toBeUndefined();
     }
   });
 
-  test(`Should return an error by length if length is not valid `, async function() {
-    const testValues = [
+  test(`Should return an error by length if length is not valid`, async function() {
+    const invalidArguments = [
       [6, 'passw'],
       [2, 'p'],
       [10, 'result'],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(invalidArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of invalidArguments) {
       const validator = validateValueLength(testValue[0]);
 
-      try {
-        await validator(testValue[1]);
-      } catch (error) {
-        expect(error.message).toBe(`Text must be more than ${testValue[0]} symbols`);
-      }
+      await expect(validator(testValue[1])).rejects.toThrow(`Text must be more than ${testValue[0]} symbols`);
     }
   });
 
   test('Should return an error if arguments is mistype', async function() {
-    const testValues = [
+    const invalidArguments = [
       [6, null],
       [2, undefined],
       [2, NaN],
@@ -49,60 +44,51 @@ describe('validateValueLength', () => {
       [10, []],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(invalidArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of invalidArguments) {
       const validator = validateValueLength(testValue[0]);
 
-      try {
-        await validator(testValue[1]);
-      } catch (error) {
-        expect(error.message).toBe(`Expected string but ${typeof testValue[1]} provided.`);
-      }
+      await expect(validator(testValue[1])).rejects.toThrow(`Expected string but ${typeof testValue[1]} provided.`);
     }
   });
 });
 
 describe('validateValueEquals', () => {
   test(`Should return resolved promise if arguments is valid.`, async function() {
-    const testValues = [
+    const validArguments = [
       ['password', 'password'],
       ['', ''],
       [' ', ' '],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(validArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of validArguments) {
       const validator = validateValueEquals(testValue[0]);
 
-      await validator(testValue[1]);
-      expect(true).toBeTruthy();
+      await expect(validator(testValue[1])).resolves.toBeUndefined();
     }
   });
 
   test(`Should return error if arguments is invalid.`, async function() {
-    const testValues = [
+    const invalidArguments = [
       ['passwor', 'password'],
       ['', 'p'],
       ['desultorine', 'desultoriness'],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(invalidArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of invalidArguments) {
       const validator = validateValueEquals(testValue[0]);
 
-      try {
-        await validator(testValue[1]);
-      } catch (error) {
-        expect(error.message).toBe(`${testValue[0]} is not equal to ${testValue[1]}`);
-      }
+      await expect(validator(testValue[1])).rejects.toThrow(`Passwords are not equal`);
     }
   });
 
   test(`Should return resolved promise is arguments is mistype.`, async function() {
-    const testValues = [
+    const invalidArguments = [
       ['passwor', null],
       ['', undefined],
       ['', NaN],
@@ -110,41 +96,36 @@ describe('validateValueEquals', () => {
       ['desultorine', []],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(invalidArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of invalidArguments) {
       const validator = validateValueEquals(testValue[0]);
 
-      try {
-        await validator(testValue[1]);
-      } catch (error) {
-        expect(error.message).toBe(`Expected string but ${typeof testValue[1]} provided.`);
-      }
+      await expect(validator(testValue[1])).rejects.toThrow(`Expected string but ${typeof testValue[1]} provided.`);
     }
   });
 });
 
 describe('validateValueWithRegex', () => {
   test(`Should return resolved promise if arguments is valid.`, async function() {
-    const testValues = [
+    const validArguments = [
       [/^[a-z\d+.\-_@]+$/, 'password'],
       [/^[a-z\d+.\-_@]+$/, 'p-a'],
       [/^[a-z\d+.\-_@]+$/, '123'],
       [/^[a-zA-Z0-9+\\._\-]{3,}@{1}[a-z]{2,}(\\.?[a-z]{1,10})?$/, 'art@sn'],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(validArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of validArguments) {
       const validator = validateValueWithRegex(testValue[0]);
 
-      await validator(testValue[1]);
-      expect(true).toBeTruthy();
+      await expect(validator(testValue[1])).resolves.toBeUndefined();
     }
   });
 
   test(`Should return resolved promise is arguments is invalid.`, async function() {
-    const testValues = [
+    const invalidArguments = [
       [/^[a-z\d+.\-_@]+$/, 'passw%%ord'],
       [/^[a-z\d+.\-_@]+$/, 'p#a'],
       [/^[a-z\d+.\-_@]+$/, '1!23'],
@@ -152,21 +133,17 @@ describe('validateValueWithRegex', () => {
       [/^[a-zA-Z0-9+\\._\-]{3,}@{1}[a-z]{2,}(\\.?[a-z]{1,10})?$/, 'a%#'],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(invalidArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of invalidArguments) {
       const validator = validateValueWithRegex(testValue[0]);
 
-      try {
-        await validator(testValue[1]);
-      } catch (error) {
-        expect(error.message).toBe(`Field is not valid`);
-      }
+      await expect(validator(testValue[1])).rejects.toThrow(`You can use only latin letters, numbers, and _,@,.,+.-`);
     }
   });
 
   test(`Should return error if arguments is mistype.`, async function() {
-    const testValues = [
+    const validArguments = [
       [/^[a-z\d+.\-_@]+$/, null],
       [/^[a-z\d+.\-_@]+$/, undefined],
       [/^[a-z\d+.\-_@]+$/, NaN],
@@ -174,16 +151,12 @@ describe('validateValueWithRegex', () => {
       [/^[a-zA-Z0-9+\\._\-]{3,}@{1}[a-z]{2,}(\\.?[a-z]{1,10})?$/, []],
     ];
 
-    expect.assertions(testValues.length);
+    expect.assertions(validArguments.length);
 
-    for (const testValue of testValues) {
+    for (const testValue of validArguments) {
       const validator = validateValueWithRegex(testValue[0]);
 
-      try {
-        await validator(testValue[1]);
-      } catch (error) {
-        expect(error.message).toBe(`Expected string but ${typeof testValue[1]} provided.`);
-      }
+      await expect(validator(testValue[1])).rejects.toThrow(`Expected string but ${typeof testValue[1]} provided.`);
     }
   });
 });
