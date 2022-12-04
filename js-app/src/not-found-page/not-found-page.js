529d32da97d9ff29ecbc71ebe58c2a12077bb59d
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2022-12-04 16:21:40 +0200

    Refactoring page

--- js-app/src/not-found-page/not-found-page.js
+++ js-app/src/not-found-page/not-found-page.js
@@ -42,14 +42,22 @@ export class NotFoundPage extends Component {
    * @inheritDoc
    * @returns {string}
    */
-  markup() {
+    markup() {
+
     return `
-   
-        <h1>404</h1>
-        <h2 class="page-not">PAGE NOT FOUND</h2>
-        
-        </div>
- 
-        `;
+        <div id="notfound">
+        <div class="notfound">
+        <div class="notfound-bg">
+        <div></div>
+    <div></div>
+    <div></div>
+  </div>
+    <h1>oops!</h1>
+    <h2>Error 404 : Page Not Found</h2>
+    <a href="#">go back</a>
+  </div>
+  </div>
+
+    `;
   }
 }
