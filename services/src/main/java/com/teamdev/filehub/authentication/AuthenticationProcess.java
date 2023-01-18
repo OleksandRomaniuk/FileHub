98e287c80cc3049513c21bce6aa0e8c9213500d7
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2023-01-18 10:51:36 +0200

    Create user authentication  command and process

---
+++ services/src/main/java/com/teamdev/filehub/authentication/AuthenticationProcess.java
@@ -0,0 +1,14 @@
+package com.teamdev.filehub.authentication;
+
+import com.teamdev.filehub.Process;
+import com.teamdev.filehub.ProcessException;
+
+/**
+ * The process for authentication users in FileHub application.
+ * The class uses data from {@link AuthenticateUserCommand} for get user token from server and check validity.
+ */
+@FunctionalInterface
+public interface AuthenticationProcess extends Process<AuthenticateUserCommand, String> {
+    @Override
+    String handle(AuthenticateUserCommand command) throws ProcessException;
+}
