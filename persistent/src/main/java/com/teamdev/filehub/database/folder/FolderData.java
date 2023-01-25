d514a52a6634257a12db4cc5cf485c4b29f58b1d
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2023-01-25 22:16:03 +0200

    Refactors

---
+++ persistent/src/main/java/com/teamdev/filehub/database/folder/FolderData.java
@@ -0,0 +1,41 @@
+package com.teamdev.filehub.database.folder;
+
+import com.google.common.base.Preconditions;
+import com.teamdev.filehub.database.Data;
+
+/**
+ * {@code FolderData} holds all information about folder for database.
+ */
+public class FolderData extends Data {
+
+    private final String name;
+
+    private String parentId;
+
+    private final String idOfOwner;
+
+    public FolderData(String id, String name, String parentId, String idOfOwner) {
+        super(id);
+        this.name = name;
+        this.parentId = parentId;
+        this.idOfOwner = idOfOwner;
+    }
+
+    public FolderData(String id, String name, String idOfOwner) {
+        super(id);
+        this.name = name;
+        this.idOfOwner = idOfOwner;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public String getIdOfOwner() {
+        return idOfOwner;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- services/src/main/java/com/teamdev/filehub/resourse/ApplicationContext.java
+++ services/src/main/java/com/teamdev/filehub/resourse/ApplicationContext.java
@@ -1,24 +1,10 @@
 package com.teamdev.filehub.resourse;
 
-import com.teamdev.filehub.authenticateduser.AuthenticatedView;
 import com.teamdev.filehub.authentication.UserAuthenticationProcess;
-import com.teamdev.filehub.delete.DeleteFileProcess;
-import com.teamdev.filehub.delete.DeleteFolderProcess;
-import com.teamdev.filehub.downaldfile.DownloadView;
-import com.teamdev.filehub.getdata.folder.GetFolderView;
-import com.teamdev.filehub.getdata.folder.content.GetFolderContentView;
-import com.teamdev.filehub.getdata.folder.content.search.GetFolderContentByNameView;
-import com.teamdev.filehub.getdata.user.GetUserDataView;
-import com.teamdev.filehub.logout.LogOutProcess;
-import com.teamdev.filehub.newfolder.CreateFolderProcess;
 import com.teamdev.filehub.registration.UserRegistrationProcess;
-import com.teamdev.filehub.renaming.file.RenamingFileProcess;
-import com.teamdev.filehub.renaming.folder.RenamingFolderProcess;
 import com.teamdev.filehub.repository.AuthenticationDao;
 import com.teamdev.filehub.repository.FileDao;
-import com.teamdev.filehub.repository.FolderDao;
 import com.teamdev.filehub.repository.UserDao;
-import com.teamdev.filehub.savefile.SaveFileProcess;
 
 public interface ApplicationContext {
 
@@ -28,11 +14,11 @@ public interface ApplicationContext {
 
     UserDao getUserDao();
 
-    SaveFileProcess getSaveProcess();
+
 
     UserAuthenticationProcess getUserAuthenticationProcess();
 
     UserRegistrationProcess getUserRegistrationProcess();
-    DownloadView getDownloadView();
+
 
 }
